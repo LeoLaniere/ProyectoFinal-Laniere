5bab83d9179d444e638bb31b6416a47fe4fab2ee
LeoLaniere <leolaniere@gmail.com>
2024-07-30 19:16:14 -0300

    Reemplazo los For Each por ciclos for y creo la funcion generar catalogo

--- js/main.js
+++ js/main.js
@@ -1,5 +1,4 @@
 //Objetos
-
 const pantalonShortSlim ={
     nombre: "Pantalon corto Slim Fit",
     color: "Negro",
@@ -18,7 +17,7 @@ const remeraOversizeNegra ={
     nombre: "Remera Overzice Negra",
     color: "Negro",
     size: "L",
-    material: "algodon",
+    material: "Algodon",
     bolsillos: 0
 }
 const remeraMusculosaRoja ={
@@ -30,57 +29,42 @@ const remeraMusculosaRoja ={
 }
 
 const listaPrendaVenta= [pantalonShortSlim, pantalonShortTheKing, remeraOversizeNegra, remeraMusculosaRoja]
-const nombresPrendas=[]
 const listaCarroCompra=[]
 
-for(const producto of listaPrendaVenta){
-    nombresPrendas.push(producto.nombre +"\n")
+function generarCatalogo(lista) {
+    let contCatalogo=0
+    let CatalogoPrenda = "\n";
+    for (var prenda of lista){
+        contCatalogo++;
+        CatalogoPrenda += "Prenda "+ contCatalogo +":\n";
+        CatalogoPrenda += "Nombre: "+ prenda.nombre+"\n";
+        CatalogoPrenda += "Color: "+ prenda.color+"\n\n";
+    }
+    return CatalogoPrenda;   
 }
-
-let CatalogoPrenda = "\n";
-listaPrendaVenta.forEach((prenda, index) => {
-    CatalogoPrenda += "Prenda "+ (index + 1) +":\n";
-    CatalogoPrenda += "Nombre: "+ (prenda.nombre)+"\n";
-    CatalogoPrenda += "Color: "+ (prenda.color) +"\n\n";
-});
-
 let menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
 while(menu !== 5) {
     switch(menu){
-        
+
         case 1:
-            alert("Catalogo de Prendas: \n" + CatalogoPrenda)
-            menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
+            alert("Catalogo de Prendas: \n" + generarCatalogo(listaPrendaVenta))
         break
 
         case 2:
-            indiceCarrito=parseInt (prompt("Agregar al carrito de compras\n"+ CatalogoPrenda))
+            indiceCarrito=parseInt (prompt("Agregar al carrito de compras\n"+ generarCatalogo(listaPrendaVenta)))
                 if (indiceCarrito >0 && indiceCarrito < listaPrendaVenta.length)
                     listaCarroCompra.push(listaPrendaVenta[indiceCarrito-1])
                 else
                     alert("Opcion invalida!")
-            menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
         break
 
         case 3:
-            let carritoPrendas = "\n";
-            listaCarroCompra.forEach((prenda, index) => {
-                carritoPrendas += "Prenda "+ (index+1) +":";
-                carritoPrendas += " "+(prenda.nombre)+"\n\n";
-            });
-            alert("Carrito de compras: \n" + carritoPrendas)
-            menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
-
+            alert("Carrito de compras: \n" + generarCatalogo(listaCarroCompra))
         break
+
         case 4:
-            let carritoQuitar = "\n";
-            listaCarroCompra.forEach((prenda, index) => {
-                carritoQuitar += "Prenda "+ (index+1) +":";
-                carritoQuitar += " "+(prenda.nombre)+"\n\n";
-            });
-            let indiceQuitar=parseInt(prompt("Quitar de carrito de compras: \n"+ carritoQuitar))
+            let indiceQuitar=parseInt(prompt("Quitar de carrito de compras: \n"+ generarCatalogo(listaCarroCompra)))
             listaCarroCompra.splice(indiceQuitar-1,1)
-            menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
         break
 
         case 5:
@@ -88,6 +72,8 @@ while(menu !== 5) {
             
         default:
             alert("opcion invalida!")
-            menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
-    }
+        }
+        
+    menu =parseInt (prompt ("Elija una opcion: \n 1-Catalogo de Prendas\n 2-Agregar al carrito de compras \n 3-Ver Carrito de Compras \n 4-Quitar del Carrito de Compras \n 5-Salir"))
+
 }
